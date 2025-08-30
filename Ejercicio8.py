1b09c49040d7bc3912517b5ca87065d75cc7a59d
aylenvieyra <aylenvieyra12@gmail.com>
2025-08-30 02:12:54 -0300

    .

--- Ejercicio8.py
+++
@@ -1,6 +0,0 @@
-def calcular_edad(año_nacimiento):
-    return 2025 - año_nacimiento
-
-anio = int(input("Ingrese su año de nacimiento: "))
-edad = calcular_edad(anio)
-print("Tu edad aproximada es:", edad)
